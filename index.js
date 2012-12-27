1718c53a18d666f115f7397b65f69fbf1a76b359
Calvin Bushor <calvinbushor@gmail.com>
2012-12-26 23:28:02 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,85 @@
+function flexFuelCalculator() {}
+
+flexFuelCalculator.prototype.calculate = function(body) {
+	var that, data;
+
+	that = this;
+	data = {
+		e85: {
+			cpm: undefined,
+			mpd: undefined
+		},
+		gas: {
+			cpm: undefined,
+			mpd: undefined
+		},
+		winner: undefined,
+		savings: {
+			money: undefined,
+			miles: undefined,
+			tank:  undefined
+		}
+	};
+
+	data['e85']['cpm'] = that.get().costPerMile(body['mpge'], body['pricee']);
+	data['e85']['mpd'] = that.get().milesPerDollar(body['mpge'], body['pricee']);
+
+	data['gas']['cpm'] = that.get().costPerMile(body['mpgg'], body['priceg']);
+	data['gas']['mpd'] = that.get().milesPerDollar(body['mpgg'], body['priceg']);
+
+	data['winner'] = that.get().winner(data['e85'], data['gas']);
+	data['savings']['money'] = that.get().savings(data['e85']['cpm'], data['gas']['cpm']);
+	data['savings']['miles'] = that.get().savings(data['e85']['mpd'], data['gas']['mpd']);
+	data['savings']['tank']  = that.get().savingsPerTank(data['savings']['money'], body['tank']);
+
+	return data;
+}
+
+flexFuelCalculator.prototype.get = function() {
+	var that, methods;
+
+	that    = this;
+	methods = {};
+
+	methods.costPerMile = function(mpg, price) {
+		var cost;
+
+		cost = (price / mpg).toFixed(3);
+
+		return cost;
+	}
+
+	methods.milesPerDollar = function(mpg, price) {
+		return (mpg / price).toFixed(3);;
+	}
+
+	methods.savings = function(a, b) {
+		if ( a >= b ) {
+			return (a - b).toFixed(3);
+		}
+
+		return (b - a).toFixed(3);
+	}
+
+	methods.savingsPerTank = function(savings, tank) {
+		return (savings * tank).toFixed(3);;
+	}
+
+	methods.winner = function(e85, gas) {
+		if ( e85['cpm'] > gas['cpm'] && e85['mpd'] < gas['mpd'] ) {
+			return 'gas';
+		} else if ( e85['cpm'] < gas['cpm'] && e85['mpd'] > gas['mpd'] ) {
+			return 'e85';
+		} else if ( e85['cpm'] < gas['cpm'] && e85['mpd'] < gas['mpd'] ) {
+			return 'less but more';
+		} else if ( e85['cpm'] > gas['cpm'] && e85['mpd'] > gas['mpd'] ) {
+			return 'more but less';
+		} else {
+			return 'tie';
+		}
+	}
+
+	return methods;
+}
+
+module.exports = new flexFuelCalculator();
